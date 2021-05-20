2716155789820912ca5808889e9efefed2e3c942
spearjohnny <spearkidume@gmail.com>
2021-05-21 01:03:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sess4local
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Age calculator on different planets");
+        }
+    }
+}
