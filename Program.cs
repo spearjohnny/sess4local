be2f93bc773f16fbe95f5dcea78996c1cec91a79
spear kidume <spearkidume@gmail.com>
2021-05-21 09:31:16 +0300

    Second Commit, a serious bug removed

--- Program.cs
+++ Program.cs
@@ -1,23 +1,17 @@
-﻿using System;
-
-namespace sess4local
+﻿namespace sess4local
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("TITLE: Age calculator on different planets");
-
+            int earthAge = 31557600, spaceAge = 631154000;
             string [] spacePlanets = {"Venus","Mars","Jupiter","Neptune"};
-            double [] orbitalPeriod = {0.61519724D,1.8808158D,11.8626615D,164.79132D};
-
-             int earthAge = 31557600, spaceAge = 631154000;
-
-            double ageInEarth = spaceAge/earthAge;    
+            double [] orbitalPeriod = {0.61519724D,1.8808158D,11.8626615D,164.79132D}, ageInEarth = spaceAge/earthAge;  
             for (int i = 0; i < spacePlanet.Length; i++)
             {
                 double ageInPlanets = ageInEarth/orbitalPeriod[i];        
-                Console.WriteLine("Earth age:" +earth_age+ "Earth-years old- " +spacePlanets[i]+":"+ageInPlanets+" Earth-years old");
+                System.Console.WriteLine("Earth age:" +ageInEarth+ "Earth-years old- " +spacePlanets[i]+":"+ageInPlanets+" Earth-years old");
         }
     }
 }
